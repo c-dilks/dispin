884b6f6f6abeb28124e65b0e7dccf1d100538ac7
christopher dilks <christopher.j.dilks@gmail.com>
2020-10-07 21:01:36 -0400

    delta fitter works

--- FitDelta.C
+++ FitDelta.C
@@ -16,15 +16,28 @@ class Injection : public TObject {
     int idx;
     float A,B;
     int idxComp;
+    TFile * infile;
+    Double_t x,y,ex,ey;
+    TGraphAsymmErrors * asymGr;
+    // - constructor
     Injection(int idx_, float A_, float B_) {
       idx = idx_;
       A = A_;
       B = B_;
       idxComp = -1;
+      TString infileN = Form("spinroot_inj_%d/asym_42.root",idx);
+      infile = new TFile(infileN,"READ");
     };
-    TString PrintVars() {
-      return Form("(%d,%.2f,%.2f)",idx,A,B);
+    // - get asymmetry for given amplitude and bin number
+    void GetAsym(Int_t amp_,Int_t binnum_) {
+      TString asymGrN = Form("kindepMA_A%d_M",amp_);
+      asymGr = (TGraphAsymmErrors*) infile->Get(asymGrN);
+      asymGr->GetPoint(binnum_,x,y);
+      ex = asymGr->GetErrorX(binnum_);
+      ey = asymGr->GetErrorY(binnum_);
     };
+    // - printout
+    TString PrintVars() { return Form("(%d,%.2f,%.2f)",idx,A,B); };
 };
 
 // return the Injection pointer for a given index
@@ -77,44 +90,42 @@ void FitDelta(Int_t amp=0, Int_t binnum=0) {
   // - calculate deltas (I minus J)
   //   - injection I with (A_I=A_J,B_I=0)
   //   - injection J with (A_J,B_J)
-  TString infileN[2];
-  TFile * infile[2];
-  TGraphAsymmErrors * asymGr[2];
   TGraph2DErrors * deltaGr = new TGraph2DErrors();
   Int_t deltaGrCnt=0;
-  Double_t x[2];
-  Double_t y[2];
-  Double_t ex[2];
-  Double_t ey[2];
-  Double_t xx,exx,yy,eyy;
-  TString asymGrN = Form("kindepMA_A%d_M",amp);
+  Double_t iv,ivErr,delta,deltaErr;
   while((inj[J] = (Injection*) nextInj())) {
     inj[I] = FindInjection(inj[J]->idxComp);
     cout << "Compare " << inj[I]->PrintVars() << 
              " minus " << inj[J]->PrintVars() << endl;
-    for(int k=0; k<2; k++) {
-      infileN[k] = Form("spinroot_inj_%d/asym_42.root",inj[k]->idx);
-      infile[k] = new TFile(infileN[k],"READ");
-      asymGr[k] = (TGraphAsymmErrors*) infile[k]->Get(asymGrN);
-      asymGr[k]->GetPoint(binnum,x[k],y[k]);
-      ex[k] = asymGr[k]->GetErrorX(binnum);
-      ey[k] = asymGr[k]->GetErrorY(binnum);
-    };
-    xx = x[I];
-    yy = y[I]-y[J];
-    exx = 0;
-    eyy = TMath::Sqrt(TMath::Abs(ey[I]*ey[I]-ey[J]*ey[J]));
-    deltaGr->SetPoint(deltaGrCnt,inj[J]->A,inj[J]->B,yy);
-    deltaGr->SetPointError(deltaGrCnt,0,0,eyy);
+
+    // get asymmetry values from I and J
+    for(int k=0; k<2; k++) inj[k]->GetAsym(amp,binnum);
+
+    // compute delta = I-J
+    iv = inj[I]->x;
+    delta = inj[I]->y - inj[J]->y;
+    ivErr = 0;
+    deltaErr = TMath::Sqrt(TMath::Abs(
+      inj[I]->ey*inj[I]->ey - inj[J]->ey*inj[J]->ey
+    ));
+
+    // set deltaGr point
+    deltaGr->SetPoint(deltaGrCnt,inj[J]->A,inj[J]->B,delta);
+    deltaGr->SetPointError(deltaGrCnt,0,0,deltaErr);
     deltaGrCnt++;
+
   };
   nextInj.Reset();
 
-  // draw delta(A,B)
+  // draw delta(A,B) and fit
   new TCanvas();
   deltaGr->SetTitle("#delta(A,B);A;B;#delta");
   deltaGr->SetMarkerColor(kBlack);
   deltaGr->SetMarkerStyle(kFullCircle);
-  deltaGr->Draw("APE");
-  deltaGr->Fit("x*y*[0]/(1+y*[0])","","");
+  deltaGr->Draw("P");
+  TF2 * func = new TF2("func","x*y*[0]/(1+y*[0])",-1,1,-1,1);
+  //TF2 * func = new TF2("func","x*y*[0]/(1+y*[1])",-1,1,-1,1);
+  deltaGr->Fit(func,"","");
+  func->Draw("surfsame");
+  cout << "chi2/ndf = " << func->GetChisquare()/func->GetNDF() << endl;
 };
