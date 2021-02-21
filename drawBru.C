b70ea3d184c83bac3d4661e420a418a591241fec
christopher dilks <christopher.j.dilks@gmail.com>
2021-02-20 19:28:09 -0500

    MCMC result drawing script: bin reading seems to work

--- drawBru.C
+++ drawBru.C
@@ -15,14 +15,43 @@ R__LOAD_LIBRARY(DiSpin)
 /////////////////////////////
 
 TObjArray * BruBins;
+Int_t nDim, nBins;
+TString bruDir;
 HS::FIT::Bins * HSbins;
+
+/////////////////////////////
+
 class BruBin : public TObject {
   public:
     Int_t idx;
     TString name,var;
-    Float_t center,mean,ub,lb;
+    Double_t center,mean,ub,lb;
+    TH1D * hist;
     // -constructor
-    BruBin() {};
+    BruBin(TAxis axis, Int_t binnum, TVectorD coord) {
+      idx = HSbins->FindBin(coord); // bin index
+      name = HSbins->GetBinName(idx); // bin name (brufit syntax)
+      var = axis.GetName(); // bin variable
+      // get bin center, lower bound, upper bound
+      center = axis.GetBinCenter(binnum);
+      lb = axis.GetBinLowEdge(binnum);
+      ub = axis.GetBinUpEdge(binnum);
+      // get bin mean (requires opening the bin's tree)
+      hist = new TH1D( Form("ivHist%d",idx),Form("ivHist%d",idx),
+        100,lb-0.05,ub+0.05);
+      TFile * binTreeFile;
+      TTree * binTree;
+      Double_t iv;
+      binTreeFile = new TFile(bruDir+"/"+name+"/TreeData.root","READ");
+      binTree = (TTree*) binTreeFile->Get("tree");
+      binTree->SetBranchAddress(var,&iv);
+      for(Long64_t e=0; e<binTree->GetEntries(); e++) {
+        binTree->GetEntry(e);
+        hist->Fill(iv);
+      };
+      mean = hist->GetMean();
+      binTreeFile->Close();
+    };
     // -print out
     void PrintInfo() {
       printf("BIN %d\n",idx);
@@ -36,49 +65,35 @@ class BruBin : public TObject {
 
 /////////////////////////////
 
-void drawBru(TString bruDir="bruspin") {
+void drawBru(TString bruDir_="bruspin") {
 
   // get nDim
+  bruDir = bruDir_;
   TFile * binFile = new TFile(bruDir+"/DataBinsConfig.root","READ");
   HSbins = (HS::FIT::Bins*) binFile->Get("HSBins");
-  Int_t nDim = HSbins->GetNAxis();
+  nDim = HSbins->GetNAxis();
   printf("nDim = %d\n",nDim);
 
   // get Nbins
-  Int_t nBins = HSbins->GetN();
+  nBins = HSbins->GetN();
   printf("nBins = %d\n",nBins);
 
   // build array of BruBin objects
   BruBins = new TObjArray();
   TVectorD binCoord(nDim);
-  BruBin * BB;
   if(nDim==1) {
-    for(auto axis0 : HSbins->GetVarAxis()) {
-      for(int b=1; b<=axis0.GetNbins(); b++) {
-        BB = new BruBin();
-        binCoord[0] = axis0.GetBinCenter(b);
-        BB->idx = HSbins->FindBin(binCoord);
-        BB->name = HSbins->GetBinName(BB->idx);
-        BB->var = axis0.GetName();
-        BB->center = axis0.GetBinCenter(b);
-        BB->mean = 0; // (updated later)
-        BB->lb = axis0.GetBinLowEdge(b);
-        BB->ub = axis0.GetBinUpEdge(b);
-        BruBins->AddLast(BB);
+    for(TAxis axis0 : HSbins->GetVarAxis()) {
+      for(int bn=1; bn<=axis0.GetNbins(); bn++) {
+        binCoord[0] = axis0.GetBinCenter(bn);
+        BruBins->AddLast(new BruBin(axis0,bn,binCoord));
       };
     };
-  }
-  else { fprintf(stderr,"ERROR: nDim too large\n"); return 1; };
-
-  // get bin means
-  TObjArrayIter nextBin(BruBins);
-  while((BB = (BruBin*) nextBin())) {
-    
   };
-  nextBin.Reset();
 
 
   // print bins
+  BruBin * BB;
+  TObjArrayIter nextBin(BruBins);
   while((BB = (BruBin*) nextBin())) {
     BB->PrintInfo();
   };
