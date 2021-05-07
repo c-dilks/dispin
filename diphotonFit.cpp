85e503a8f941d43fb7fb41bb005984b62cdeba3f
christopher dilks <christopher.j.dilks@gmail.com>
2021-05-07 17:52:12 -0400

    writing diphTr fitter

--- diphotonFit.cpp
+++ diphotonFit.cpp
@@ -12,6 +12,7 @@
 #include <TH1.h>
 #include <TTree.h>
 #include <TIterator.h>
+#include <TGraphErrors.h>
 
 // RooFit
 #include <RooGlobalFunc.h>
@@ -58,7 +59,6 @@ class FitBin {
     TString binStr;
     RooFitResult *modelFit;
     RooPlot *plotFrame;
-    TCanvas *fitCanv;
 
     // constructor -------------------------------------
     FitBin(Int_t binn) {
@@ -82,21 +82,23 @@ class FitBin {
           mass,RooFit::Import(*Mdist));
 
       // pi0 signal
+      // WARNING: do not use underscores in param names
       Double_t nmax = (Double_t)ENT;
-      RooRealVar pi0n(("pi0n"+binStr).Data(),"pi0n", nmax/2.0, 0, nmax);
-      RooRealVar pi0mu(("pi0mu"+binStr).Data(),"pi0mu", 0.135, 0, 2);
-      RooRealVar pi0sigma(("pi0sigma"+binStr).Data(),"pi0sigma", 0.02, 0.001, 0.1);
+      nmax /= BS->GetNbinsTotal();
+      RooRealVar pi0n(("pi0n"+binStr).Data(),"#pi^{0} N", nmax/2.0, 0, nmax);
+      RooRealVar pi0mu(("pi0mu"+binStr).Data(),"#pi^{0} M", 0.135, 0, 2);
+      RooRealVar pi0sigma(("pi0sigma"+binStr).Data(),"#pi^{0} #sigma", 0.02, 0.001, 0.1);
       RooGaussian pi0model(("pi0model"+binStr).Data(),"pi0model",mass,pi0mu,pi0sigma);
 
       // background
-      RooRealVar bgN(("bgN"+binStr).Data(),"bgN", 0, nmax);
-      RooRealVar bgP0(("bgP0"+binStr).Data(),"bgP0", -1, 1);
-      RooRealVar bgP1(("bgP1"+binStr).Data(),"bgP1", -1, 1);
-      RooRealVar bgP2(("bgP2"+binStr).Data(),"bgP2", -1, 1);
-      RooRealVar bgP3(("bgP3"+binStr).Data(),"bgP3", -1, 1);
-      RooRealVar bgP4(("bgP4"+binStr).Data(),"bgP4", -1, 1);
-      RooPolynomial bgModel(("bgModel"+binStr).Data(),"bgModel",mass,RooArgSet(bgP0,bgP1,bgP2));
-      //RooChebychev bgModel(("bgModel"+binStr).Data(),"bgModel",mass,RooArgSet(bgP0,bgP1,bgP2,bgP3));
+      RooRealVar bgN(("bgN"+binStr).Data(),"BG N", 0, nmax);
+      RooRealVar bgP0(("bgP0"+binStr).Data(),"BG p_{0}", -1, 1);
+      RooRealVar bgP1(("bgP1"+binStr).Data(),"BG p_{1}", -1, 1);
+      RooRealVar bgP2(("bgP2"+binStr).Data(),"BG p_{2}", -1, 1);
+      RooRealVar bgP3(("bgP3"+binStr).Data(),"BG p_{3}", -1, 1);
+      RooRealVar bgP4(("bgP4"+binStr).Data(),"BG p_{4}", -1, 1);
+      //RooPolynomial bgModel(("bgModel"+binStr).Data(),"bgModel",mass,RooArgSet(bgP0,bgP1,bgP2));
+      RooChebychev bgModel(("bgModel"+binStr).Data(),"bgModel",mass,RooArgSet(bgP0,bgP1,bgP2,bgP3));
       //RooChebychev bgModel(("bgModel"+binStr).Data(),"bgModel",mass,RooArgSet(bgP0,bgP1,bgP2,bgP3,bgP4));
 
       // signal+bg
@@ -117,8 +119,11 @@ class FitBin {
           );
 
       // plot
+      TString massdistT = "M_{#gamma#gamma} distribution";
+      for(int d=0; d<BS->dimensions; d++)
+        massdistT += ", " + BS->GetBoundStr(binnum,d);
       plotFrame = mass.frame(
-          RooFit::Title("M_{#gamma#gamma} distribution")
+          RooFit::Title(massdistT)
           );
       massdist.plotOn(plotFrame,
           RooFit::Name(("massdistPlot"+binStr).Data()),
@@ -147,16 +152,7 @@ class FitBin {
           RooFit::LineWidth(4),
           RooFit::LineStyle(9)
           );
-      fitCanv = new TCanvas(
-          "fitCanv"+binStr,"fitCanv"+binStr,
-          1200,800);
-      plotFrame->Draw();
-      Mdist->SetMarkerStyle(kFullCircle);
-      Mdist->SetMarkerSize(1);
-      Mdist->SetMarkerColor(kBlack);
-      Mdist->Draw("EPSAME");
     };
-
 };
 
 
@@ -251,35 +247,102 @@ int main(int argc, char** argv) {
   for(Int_t bn : BS->binVec) FitBinList.at(bn)->Fit();
 
 
-  // get fit parameters 
+  // loop through fit parameters and results
+  // TODO: paramGr is only good for 1D binning schemes;
+  // quick fix: for multidim, could just plot params vs. binnum
   for(int i=0; i<3; i++) Tools::PrintSeparator(60,"=");
   Tools::PrintTitleBox("FIT RESULTS");
+  Bool_t first = true;
+  map<TString,TGraphErrors*> paramGrList;
+  TGraphErrors *paramGr;
+  TString parName;
+  Int_t ptCnt = 0;
+  Int_t nParam = 0;;
   for(Int_t bn : BS->binVec) {
+
     fb = FitBinList.at(bn);
     cout << "BIN " << fb->binnum
          << " -- " << BS->IVname[0]
          << " = " << fb->IVdist->GetMean()
          << endl;
+
     auto parList = fb->modelFit->floatParsFinal();
     parList.sort();
     TIterator *nextPar = parList.createIterator();
+
+    // loop over fit params
     while(RooRealVar *par = (RooRealVar*) (*nextPar)()) {
-      cout << "  " << par->GetName() << " = "
+
+      // print
+      parName = TString(par->GetName());
+      parName(TRegexp("_.*$"))="";
+      cout << "  " << parName << " = "
                    << par->getVal() << " +/- "
                    << par->getError()
                    << endl;
+
+      // instantiate graph
+      if(first) {
+        paramGr = new TGraphErrors();
+        paramGr->SetName("paramGr_"+parName);
+        paramGr->SetTitle(TString(par->GetTitle())+" vs. "+BS->IVname[0]);
+        paramGrList.insert(pair<TString,TGraphErrors*>(parName,paramGr));
+        nParam++;
+      } else {
+        paramGr = paramGrList.at(parName);
+      };
+
+      paramGr->SetPoint(ptCnt,fb->IVdist->GetMean(),par->getVal());
+      paramGr->SetPointError(ptCnt,0,par->getError());
     };
+    first = false;
+    ptCnt++;
   };
 
+  // parameter canvas
+  Int_t ncols = 4;
+  Int_t nrows = (nParam-1)/ncols+1;
+  TCanvas *paramCanv = new TCanvas("paramCanv","paramCanv",400*ncols,300*nrows);
+  paramCanv->Divide(ncols,nrows);
+  Int_t pad=1;
+  for(auto & parKV : paramGrList) {
+    paramGr = parKV.second;
+    paramCanv->cd(pad);
+    paramGr->SetMarkerStyle(kFullCircle);
+    paramGr->SetMarkerColor(kBlack);
+    paramGr->Draw("APE");
+    pad++;
+  };
+
+  // fit results canvas
+  nrows = (BS->GetNbinsTotal()-1)/ncols+1;
+  TCanvas *fitCanv = new TCanvas("fitCanv","fitCanv",400*ncols,300*nrows);
+  fitCanv->Divide(ncols,nrows);
+  pad=1;
+  for(Int_t bn : BS->binVec) {
+    fb = FitBinList.at(bn);
+    fitCanv->cd(pad++);
+    fb->plotFrame->Draw();
+    fb->Mdist->SetMarkerStyle(kFullCircle);
+    fb->Mdist->SetMarkerSize(0.5);
+    fb->Mdist->SetMarkerColor(kBlack);
+    fb->Mdist->Draw("EPSAME");
+  };
 
 
   // write
   outfile->cd();
+  fitCanv->Write();
+  paramCanv->Write();
   for(Int_t bn : BS->binVec) {
     fb = FitBinList.at(bn);
     fb->Mdist->Write();
-    fb->fitCanv->Write();
   };
+
+
+  // print canvas (for quick look)
+  fitCanv->Print("diphotonFitResultCanv.png");
+  paramCanv->Print("diphotonFitParamCanv.png");
 
 
   // cleanup
