51756e0696b91a2e27e66d0226ef7f8bc950123e
christopher dilks <christopher.j.dilks@gmail.com>
2021-10-04 11:24:53 -0400

    modified:   analysis_pi0_compare.sh

--- analysis_pi0_compare.sh
+++ analysis_pi0_compare.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
 # compare results from analysis_pi0.sh, e.g., sFit vs. sideband method
 
-if [ $# -ne 1 ]; then
-  echo "USAGE: $0 [varName]"
+if [ $# -ne 2 ]; then
+  echo "USAGE: $0 [varName] [testNum]"
   exit 1
 fi
 varName=$1
+testNum=$2
 
 minimizer="minuit"
 
-bluefile="bruspin.sfit.${varName}/asym_${minimizer}_BL0.root"
-redfile="bruspin.pi0.sig.${varName}/asymBGcorr_${minimizer}_BL0.root"
-bluetitle="sFit method"
-redtitle="sideband method"
+case $testNum in
+  1)
+    bluetitle="sFit method"
+    redtitle="sideband method"
+    bluefile="bruspin.sfit.${varName}/asym_${minimizer}_BL0.root"
+    redfile="bruspin.pi0.sig.${varName}/asymBGcorr_${minimizer}_BL0.root"
+    ;;
+  2)
+    bluetitle="sFit method"
+    redtitle="no BG correction"
+    bluefile="bruspin.sfit.${varName}/asym_${minimizer}_BL0.root"
+    redfile="bruspin.pi0.sig.${varName}/asym_${minimizer}_BL0.root"
+    ;;
+  3)
+    bluetitle="sideband BG correction"
+    redtitle="no BG correction"
+    bluefile="bruspin.pi0.sig.${varName}/asymBGcorr_${minimizer}_BL0.root"
+    redfile="bruspin.pi0.sig.${varName}/asym_${minimizer}_BL0.root"
+    ;;
+  4)
+    bluetitle="sFit using BG weights"
+    redtitle="regular fit of sideband data"
+    bluefile="bruspin.sbg.${varName}/asym_${minimizer}_BL0.root"
+    redfile="bruspin.pi0.bg.${varName}/asym_${minimizer}_BL0.root"
+    ;;
+  *)
+    echo "bad testNum"
+    exit 1
+    ;;
+esac
 
-#bluefile="bruspin.sbg.${varName}/asym_${minimizer}_BL0.root"
-#redfile="bruspin.pi0.bg.${varName}/asym_${minimizer}_BL0.root"
-#bluetitle="sFit using BG weights"
-#redtitle="regular fit of sideband data"
-
-root -l -b -q CompareBruAsym.C'("'"$bluetitle"'","'$bluefile'","'"$redtitle"'","'$redfile'")'
+root -l CompareBruAsym.C'("'"$bluetitle"'","'$bluefile'","'"$redtitle"'","'$redfile'")'
