50c2a045ef9ddf423a82c1c73be7e3d8b0749d37
christopher dilks <christopher.j.dilks@gmail.com>
2022-01-26 18:57:05 -0500

    modified:   singleBinTreeMaker.sh and waitForSlurm.sh

--- waitForSlurm.sh
+++ waitForSlurm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # runs squeue in a loop which does not exit until no jobs
 # remain in the slurm queue
+# - assume optional additional args is job name(s)
 
 while [ 1 ]; do
   if [ $# -gt 0 ]; then
-    n=$(squeue -r -u $(whoami) | grep $* | wc -l)
+    n=$(squeue -r -u $(whoami) -n $* | wc -l)
   else
     n=$(squeue -r -u $(whoami) | wc -l)
-    let n--
   fi
+  let n--
   if [ $n -gt 0 ]; then echo "$n slurm jobs remain..."; sleep 30
   else echo "slurm done"; break
   fi
