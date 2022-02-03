68254a1152005ecfd7bc6df36d3652c52cc55f57
christopher dilks <christopher.j.dilks@gmail.com>
2022-02-03 11:27:55 -0500

    modified:   finalFit.rb

--- finalFit.rb
+++ finalFit.rb
@@ -2,51 +2,57 @@
 # run asymBruFit.C with arguments for the final fit
 
 # settings #################
-idString  = "test"
-datasets = ["rga", "rgb"]
-ivBinning = { # map `ivType` to number of bins for each dimension
-  1  => [6],
-  2  => [6],
+idString    = "final.feb3"
+datasets    = ["rga", "rgb"]
+minimizers  = ["minuit", "mcmccov"]
+ivBinnings  = { # map `ivType` to number of bins for each dimension
+  1  => [12],
+  2  => [12],
   32 => [4,3],
   42 => [4,3],
 }
-minimizers = ["minuit", "mcmccov"]
-nCPUs     = 6   # number of CPUs per node to allocate for slurm # TODO: determine from number of bins
-timeLim   = 48 # time limit [hr] # TODO: minimizer.contains? "mcmc" ? 48 : 5
+timeLim = 36 # time limit [hr]
 ############################
 
+sep = Proc.new{ |title| puts "\n#{title}\n"+"="*40 }
+
 # if on ifarm, use slurm; otherwise, run sequentially
+sep.call "job settings"
 slurm = `hostname`.chomp.include? "ifarm"
 puts slurm ?
-  "MODE: on ifarm, run on CLUSTER with SLURM" :
-  "MODE: not on ifarm, run SEQUENTIALLY"
+  "Mode: on ifarm, run on cluster with Slurm" :
+  "Mode: not on ifarm, run sequentially"
 
-# make brudir names
-brudirs = datasets.map{ |d| [idString,dataset,minimizer,iv].join "." } # AQUI: make this a proc?
-
-# argsets: list of sets of arguments; each "argset" will be one job
-sep = Proc.new{ |title| puts "\n#{title}\n"+"="*40 }
-argsets = datasets.product( ivBinning.keys, minimizers )
-sep.call "ARGSETS"
-argsets.each{|a| puts "#{a}"}
-exit
-
-
+# determine number of CPUs to allocate per slurm node
+numBins = ivBinnings.values.map{ |nbins| nbins.inject :* }
+nCPUs = numBins.max # use max number of bins from any job -> could waste resources if some jobs have less bins
+# nCPUs = numBins.min # use min number of bins from any job -> jobs with more bins will take longer
+if slurm
+  puts "Requesting #{nCPUs} CPUs per slurm node"
+  $stderr.puts "\nWARNING: some jobs have more/less bins than others, impacting resource usage (see #{$0})\n\n" if numBins.uniq.length>1
+end
 
 # start job list file
-jobFileName = "jobs.asymBruFit.#{minimizer}.#{ivString}.slurm"
+jobFileName = "jobs.#{idString}.slurm"
 slurmFileName = jobFileName.gsub(/^jobs/,"job") if slurm
 jobFile = File.open(jobFileName,"w")
+jobFile.puts "#!/bin/bash" unless slurm
 
-# define asymBruFit.C call, and append to job list
-fit = Proc.new do |whichSpinMC|
+# reformat some settings
+ivTypes = ivBinnings.keys
+ivBinnings.each{ |_,nbins| nbins.replace (nbins+[-1,-1])[0..2] } # fill rest of nbins Arrays with default `-1` values
+
+# loop over all possible settings, defining asymBruFit.C calls, and generate job list
+# - ASSUME there will be one job per possible setting
+sep.call "job list"
+settings = datasets.product(ivTypes,minimizers).each do |dataset,ivType,minimizer|
+  bruDir = "bruspin.volatile/" + [idString,dataset,ivType,minimizer].join('.')
   bruArgs = [
-    "bruspin.volatile/bruspin.#{minimizer}.#{ivString}.inj#{whichSpinMC}",
+    bruDir,
     minimizer,
     "",
     ivType,
-    *nbins,
-    whichSpinMC,
+    *ivBinnings[ivType],
   ]
   bruArgs.map!{ |arg| if arg.class==String then "\"#{arg}\"" else arg end } # add quotes around strings
   brufit = "root -b -q $BRUFIT/macros/LoadBru.C"
@@ -55,10 +61,6 @@ fit = Proc.new do |whichSpinMC|
     "#{brufit} 'asymBruFit.C(#{bruArgs.join ','})'"
   [$stdout,jobFile].each{ |s| s.puts cmd }
 end
-
-# generate job list
-jobFile.puts "#!/bin/bash" unless slurm
-injSeq.each{ |i| fit.call i } # loop
 jobFile.close
 
 # generate slurm file
@@ -66,12 +68,12 @@ if slurm
   slurmFile = File.open(slurmFileName,"w") if slurm
   slurmSet = Proc.new{ |var,val| slurmFile.puts "#SBATCH --#{var}=#{val}" }
   slurmFile.puts "#!/bin/bash"
-  slurmSet.call("job-name",      "asymBruFit")
+  slurmSet.call("job-name",      "#{idString}")
   slurmSet.call("account",       "clas12")
   slurmSet.call("partition",     "production")
   slurmSet.call("mem-per-cpu",   "#{7200/nCPUs}")
   slurmSet.call("time",          "#{timeLim}:00:00")
-  slurmSet.call("array",         "1-#{injSeq.length}")
+  slurmSet.call("array",         "1-#{settings.length}")
   slurmSet.call("ntasks",        "1")
   slurmSet.call("cpus-per-task", "#{nCPUs}")
   slurmSet.call("output",        "/farm_out/%u/%x-%A_%a.out")
@@ -82,12 +84,12 @@ end
 
 # execution
 if slurm # run on slurm
-  puts "-"*30
+  sep.call "slurm script"
   system "cat #{slurmFileName}"
-  puts "-"*30
-  puts "submitting to slurm..."
+  sep.call "launch"
   system "sbatch #{slurmFileName}"
 else # run locally (sequentially)
+  sep.call "launch"
   system "chmod u+x #{jobFileName}"
   system "./#{jobFileName}"
 end
