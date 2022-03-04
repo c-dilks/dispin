e502b650fe9460188fbb42e696dc8b508bc51b57
christopher dilks <christopher.j.dilks@gmail.com>
2022-03-04 17:01:36 -0500

    modified:   buildBrufit.sh

--- buildBrufit.sh
+++ buildBrufit.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 # build brufit (clean)
+set -e
 pushd deps/brufit
 mkdir -p build
 rm -r build
 mkdir -p build
-cd build
-cmake -DCMAKE_C_COMPILER=$(which gcc) ../ && \
-  make install && \
-  cmake -DCMAKE_C_COMPILER=$(which gcc) ../ && \
-  make install && \
-  echo """
-  build finished
-  IMPORTANT: run a brufit test fit at least once to build any remaining
-             shared libraries (e.g. PDFExpand)
-  """ && \
-  popd && exit 0
+pushd build
+cmake -DCMAKE_C_COMPILER=$(which gcc) ../
+make install
+cmake -DCMAKE_C_COMPILER=$(which gcc) ../
+make install
+echo """
+build finished!
+
+>>> now running a brufit tutorial to build remaining
+    shared libraries (e.g. PDFExpand)...
+
+"""
 popd
-exit 1
+pushd tutorials/SphHarmonic
+root -b -q $BRUFIT/macros/LoadBru.C $* GenSphHarmonicMoments.C
+popd
+popd
+echo "DONE"
