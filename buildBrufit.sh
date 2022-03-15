c4d36d7e5d307b9600c96dcbb3746f22f65005bb
christopher dilks <christopher.j.dilks@gmail.com>
2022-03-15 12:40:49 -0400

    modified:   buildBrufit.sh

--- buildBrufit.sh
+++ buildBrufit.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 # build brufit (clean)
 set -e
-pushd deps/brufit
-mkdir -p build
-rm -r build
+
+if [ -z "$BRUFIT" ]; then
+  echo "ERROR: BRUFIT env var not set"
+  exit 1
+fi
+pushd $BRUFIT
+
+# clean
+echo "CLEANING..."
+rm -rfv build
+rm -fv macros/*.so
+
+# build main shared lib
 mkdir -p build
 pushd build
 cmake -DCMAKE_C_COMPILER=$(which gcc) ../
 make install
 cmake -DCMAKE_C_COMPILER=$(which gcc) ../
 make install
+popd
+
+# build aux shared lib(s)
 echo """
 build finished!
 
@@ -17,9 +30,10 @@ build finished!
     shared libraries (e.g. PDFExpand)...
 
 """
-popd
 pushd tutorials/SphHarmonic
 root -b -q $BRUFIT/macros/LoadBru.C $* GenSphHarmonicMoments.C
 popd
+
+# finish
 popd
 echo "DONE"
