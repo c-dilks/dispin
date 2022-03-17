2cbdc472ce6c81bacb018e29340d2670f994656d
Christopher Dilks <c-dilks@users.noreply.github.com>
2022-03-17 12:00:33 -0400

    debug sporadic segfaults from brufit (#21)
    * modified:   buildBrufit.sh
    
    * merge slurm stderr and stdout
    
    * modified:   buildBrufit.sh
    
    * revert 603e62a merge slurm stderr and stdout
    
    * modified:   errorPrint.sh

--- buildBrufit.sh
+++ buildBrufit.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 # build brufit (clean)
-pushd deps/brufit
+set -e
+
+if [ -z "$BRUFIT" ]; then
+  echo "ERROR: BRUFIT env var not set"
+  exit 1
+fi
+pushd $BRUFIT
+
+# clean
+echo "CLEANING..."
+rm -rfv build
+rm -fv macros/*.so
+
+# build main shared lib
 mkdir -p build
-rm -r build
-mkdir -p build
-cd build
-cmake -DCMAKE_C_COMPILER=$(which gcc) ../ && \
-  make install && \
-  cmake -DCMAKE_C_COMPILER=$(which gcc) ../ && \
-  make install && \
-  echo """
-  build finished
-  IMPORTANT: run a brufit test fit at least once to build any remaining
-             shared libraries (e.g. PDFExpand)
-  """ && \
-  popd && exit 0
+pushd build
+cmake -DCMAKE_C_COMPILER=$(which gcc) ../
+make install
+cmake -DCMAKE_C_COMPILER=$(which gcc) ../
+make install
 popd
-exit 1
+
+# build aux shared lib(s)
+echo """
+build finished!
+
+>>> now running a brufit tutorial to build remaining
+    shared libraries (e.g. PDFExpand)...
+
+"""
+pushd tutorials/SphHarmonic
+root -b -q $BRUFIT/macros/LoadBru.C $* GenSphHarmonicMoments.C
+popd
+
+# finish
+popd
+echo "DONE"
