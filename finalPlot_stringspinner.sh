fc4776ea3ece8b1eb820c2782a38db8e0a793fa4
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-01-09 16:55:45 -0500

    fix: bash quotation quirks

--- finalPlot_stringspinner.sh
+++ finalPlot_stringspinner.sh
@@ -8,22 +8,10 @@ fi
 setnum=$1
 
 case $setnum in
-  1)
-    sssDir="bruspin/stringspinner.pm.mcgen.x.minuit"
-    clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.x.minuit"
-    ;;
-  2)
-    sssDir="bruspin/stringspinner.pm.mcgen.m.minuit"
-    clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.m.minuit"
-    ;;
-  3)
-    sssDir="bruspin/stringspinner.pm.mcgen.zm.minuit"
-    clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.zm.minuit"
-    ;;
-  4)
-    sssDir="bruspin/stringspinner.pm.mcgen.ptm.minuit"
-    clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.ptm.minuit"
-    ;;
+  1) blList=(0) ;;
+  2) blList=(0) ;;
+  3) blList=(0 1) ;;
+  4) blList=(0 1) ;;
   *)
     echo "ERROR: unknown set_number" >&2
     exit 1
@@ -31,9 +19,38 @@ case $setnum in
 esac
 
 for s in 2 3; do
-  pwPlot.py \
-    -s $s \
-    -l 'StringSpinner;CLAS12' \
-    $sssDir/asym_minuit_BL0.root \
-    $clasDir/asym_minuit_BL0.root
+  for bl in ${blList[*]}; do
+
+    case $setnum in
+      1)
+        sssDir="bruspin/stringspinner.pm.mcgen.x.minuit"
+        clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.x.minuit"
+        titleArgs=(-x '$x$')
+        ;;
+      2)
+        sssDir="bruspin/stringspinner.pm.mcgen.m.minuit"
+        clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.m.minuit"
+        titleArgs=(-x '$M_h$ [GeV]')
+        ;;
+      3)
+        sssDir="bruspin/stringspinner.pm.mcgen.zm.minuit"
+        clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.zm.minuit"
+        titleArgs=(-x '$z$' -e '$M_h$ bin '$bl)
+        ;;
+      4)
+        sssDir="bruspin/stringspinner.pm.mcgen.ptm.minuit"
+        clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.ptm.minuit"
+        titleArgs=(-x '$p_T$ [GeV]' -e '$M_h$ bin '$bl)
+        ;;
+    esac
+
+    pwPlot.py                        \
+      -s $s                          \
+      -l 'StringSpinner;CLAS12'      \
+      "${titleArgs[@]}"              \
+      $sssDir/asym_minuit_BL$bl.root \
+      $clasDir/asym_minuit_BL$bl.root
+
+  done
 done
+
