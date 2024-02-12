120e6fe390802076c1e00ba381b20bb3f811d329
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-02-12 18:17:09 -0500

    feat: better `legend.rb` (#64)

--- legend.rb
+++ legend.rb
@@ -8,11 +8,33 @@ r.gROOT.SetBatch true
 legend = r.TLegend.new 0.1, 0.1, 0.9, 0.9
 
 graphs = {
-  :rga => { :title=>'Proton Target',   :style=>r.kFullCircle, :color=>r.kRed-7  },
-  :rgb => { :title=>'Deuteron Target', :style=>r.kFullSquare, :color=>r.kBlue+3 },
-  :mc  => { :title=>'Monte Carlo',     :style=>r.kOpenCircle, :color=>r.kBlack  },
+  :asymmetry => {
+    :rga => { :title=>'Proton Target',   :style=>r.kFullCircle, :color=>r.kRed-7  },
+    :rgb => { :title=>'Deuteron Target', :style=>r.kFullSquare, :color=>r.kBlue+3 },
+    :mc  => { :title=>'Monte Carlo',     :style=>r.kOpenCircle, :color=>r.kBlack  },
+  },
+  :CompareCatTreeDists => {
+    :a => { :title=>'StringSpinner', :style=>r.kFullTriangleUp,   :color=>r.kGreen+1  },
+    :b => { :title=>'CLAS12 MCgen',  :style=>r.kFullTriangleDown, :color=>r.kViolet+2 },
+  },
 }
-graphs.each do |key,hash|
+
+if ARGV.empty?
+  $stderr.puts "USAGE: #{$0} [key]"
+  $stderr.puts " keys:"
+  graphs.each do |key,hash|
+    $stderr.puts "   - #{key.to_s}"
+  end
+  exit 2
+end
+user_key = ARGV[0].to_sym
+
+unless graphs.has_key? user_key
+  $stderr.puts "ERROR: key '#{user_key}' not defined"
+  exit 1
+end
+
+graphs[user_key].each do |key,hash|
   hash[:graph] = r.TGraph.new
   hash[:graph].SetMarkerStyle hash[:style]
   hash[:graph].SetMarkerColor hash[:color]
