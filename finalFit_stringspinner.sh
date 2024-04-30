2609f1d595cf3f17822c0943c5bf37c66e910fd0
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-04-30 17:46:16 -0400

    test: vary stringspinner free parameters
    _e.g._, relative fractions of VM longitudinal to transverse polarization

--- finalFit_stringspinner.sh
+++ finalFit_stringspinner.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # fit stringspinner data
 
-if [ $# -lt 1 ]; then
-  echo "USAGE: $0 [set_number]" >&2
+if [ $# -lt 2 ]; then
+  echo """
+  USAGE: $0 [set_number] [prefix]
+
+    [set_number]: defines the binning scheme; see the script
+    [prefix]:     directory name prefix, e.g., stringspinner.GLGT_5.thetaLT_0
+  """ >&2
   exit 2
 fi
 setnum=$1
+prefix=$2
 
 case $setnum in
-  1) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/stringspinner.pm.mcgen.x.minuit", "minuit", "", 0x34, 1, 6)' ;;
-  2) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/stringspinner.pm.mcgen.m.minuit", "minuit", "", 0x34, 2, 6)' ;;
-  3) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/stringspinner.pm.mcgen.zm.minuit", "minuit", "", 0x34, 32, 3, 2)' ;;
-  4) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/stringspinner.pm.mcgen.ptm.minuit", "minuit", "", 0x34, 42, 3, 2)' ;;
+  1) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.x.minuit", "minuit", "", 0x34, 1, 6)' ;;
+  2) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.m.minuit", "minuit", "", 0x34, 2, 6)' ;;
+  3) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.zm.minuit", "minuit", "", 0x34, 32, 3, 2)' ;;
+  4) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.ptm.minuit", "minuit", "", 0x34, 42, 3, 2)' ;;
   *)
     echo "ERROR: unknown set_number" >&2
     exit 1
