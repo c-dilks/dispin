63021d621e405a615429e0d416caba1d05b150da
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-04-30 17:46:16 -0400

    test: vary stringspinner free parameters
    _e.g._, relative fractions of VM longitudinal to transverse polarization

--- finalPlot_stringspinner.sh
+++ finalPlot_stringspinner.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # plot stringspinner asymmetries compared to CLAS12 results
 
-if [ $# -lt 1 ]; then
-  echo "USAGE: $0 [set_number]" >&2
+if [ $# -lt 2 ]; then
+  echo """
+  USAGE: $0 [set_number] [prefix]
+
+    [set_number]: defines the binning scheme; see the script
+    [prefix]:     directory name prefix, e.g., stringspinner.GLGT_5.thetaLT_0
+  """ >&2
   exit 2
 fi
 setnum=$1
+prefix=$2
 
 case $setnum in
   1) blList=(0) ;;
@@ -23,22 +29,22 @@ for s in 2 3; do
 
     case $setnum in
       1)
-        sssDir="bruspin/stringspinner.pm.mcgen.x.minuit"
+        sssDir="bruspin/$prefix.pm.mcgen.x.minuit"
         clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.x.minuit"
         titleArgs=(-x '$x$')
         ;;
       2)
-        sssDir="bruspin/stringspinner.pm.mcgen.m.minuit"
+        sssDir="bruspin/$prefix.pm.mcgen.m.minuit"
         clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.m.minuit"
         titleArgs=(-x '$M_h$ [GeV]')
         ;;
       3)
-        sssDir="bruspin/stringspinner.pm.mcgen.zm.minuit"
+        sssDir="bruspin/$prefix.pm.mcgen.zm.minuit"
         clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.zm.minuit"
         titleArgs=(-x '$z$' -e '$M_h$ bin '$bl)
         ;;
       4)
-        sssDir="bruspin/stringspinner.pm.mcgen.ptm.minuit"
+        sssDir="bruspin/$prefix.pm.mcgen.ptm.minuit"
         clasDir="bruspin/TRANSVERSITY2022.rga.pm.bibending.all.ptm.minuit"
         titleArgs=(-x '$p_T$ [GeV]' -e '$M_h$ bin '$bl)
         ;;
