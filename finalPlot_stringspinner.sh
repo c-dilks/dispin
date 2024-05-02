bb70c71f80575d633e472f7c85611d30c3bf0be2
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-05-02 13:36:29 -0400

    style: plots

--- finalPlot_stringspinner.sh
+++ finalPlot_stringspinner.sh
@@ -62,7 +62,7 @@ for s in 2 3; do
 
     pwPlot.py                             \
       -s $s                               \
-      -l "'$legendLabels'"                \
+      -l "$legendLabels"                  \
       "${titleArgs[@]}"                   \
       "${sssFiles[@]}"                    \
       $clasDir/asym_minuit_BL$bl.root
