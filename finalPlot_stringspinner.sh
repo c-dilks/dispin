76e388aeaaba15cdb006a61d8644dcbc5fec00c6
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-05-02 12:48:31 -0400

    fix: catTree prefix

--- finalPlot_stringspinner.sh
+++ finalPlot_stringspinner.sh
@@ -6,7 +6,7 @@ if [ $# -lt 2 ]; then
   USAGE: $0 [set_number] [prefixes]...
 
     [set_number]:   defines the binning scheme; see the script
-    [prefixes]...:  directory name prefix, e.g., stringspinner.GLGT_5.thetaLT_0
+    [prefixes]...:  directory name prefix, e.g., sss.GLGT_5.thetaLT_0
                     (specify multiple, if you want)
   """ >&2
   exit 2
