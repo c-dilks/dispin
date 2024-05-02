8f7bad2c4d9e3339760c2e7e8d80d71d4d74c2de
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-05-02 12:48:31 -0400

    fix: catTree prefix

--- finalFit_stringspinner.sh
+++ finalFit_stringspinner.sh
@@ -14,10 +14,10 @@ setnum=$1
 prefix=$2
 
 case $setnum in
-  1) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.x.minuit", "minuit", "", 0x34, 1, 6)' ;;
-  2) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.m.minuit", "minuit", "", 0x34, 2, 6)' ;;
-  3) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.zm.minuit", "minuit", "", 0x34, 32, 3, 2)' ;;
-  4) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.sss.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.ptm.minuit", "minuit", "", 0x34, 42, 3, 2)' ;;
+  1) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.'$prefix'.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.x.minuit", "minuit", "", 0x34, 1, 6)' ;;
+  2) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.'$prefix'.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.m.minuit", "minuit", "", 0x34, 2, 6)' ;;
+  3) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.'$prefix'.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.zm.minuit", "minuit", "", 0x34, 32, 3, 2)' ;;
+  4) root -b -q $BRUFIT/macros/LoadBru.C asymBruFit.C'("catTrees/catTreeData.'$prefix'.0x34.idx.root", "", "bruspin/'$prefix'.pm.mcgen.ptm.minuit", "minuit", "", 0x34, 42, 3, 2)' ;;
   *)
     echo "ERROR: unknown set_number" >&2
     exit 1
