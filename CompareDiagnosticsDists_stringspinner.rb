96cdc492e09ae7a330fd105ed25651c1633f5c2a
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-07-10 19:38:33 -0400

    modified:   CompareDiagnosticsDists_stringspinner.rb

--- CompareDiagnosticsDists_stringspinner.rb
+++ CompareDiagnosticsDists_stringspinner.rb
@@ -5,6 +5,9 @@ require 'awesome_print'
 require 'pycall/import'
 r = PyCall.import_module 'ROOT'
 
+r.gStyle.SetOptStat 0
+r.gROOT.SetBatch true
+
 # NOTE: this script takes NO ARGUMENTS, instead, see the following
 # to decide the input files, their plot styles, etc.
 ##################################################################################
@@ -51,20 +54,29 @@ ap file_list
 # list of plots we want to draw
 plot_list = [
   'MhDist',
+  'Q2Dist',
+  'XDist',
+  'PhPerpDist',
+  'ZpairDist',
+  'PhiHDist',
+  'PhiRDist',
+  'thetaDist',
 ].map do |plot_name|
   {
     :name => plot_name,
-    :canv => r.TCanvas.new("canv_#{plot_name}", "canv_#{plot_name}", 800, 600),
+    :canv => r.TCanvas.new("canv_#{plot_name}", "canv_#{plot_name}", 2400, 1800),
   }
 end
 
 # make plots
-first = true
+legend = r.TLegend.new 0.1, 0.1, 0.9, 0.9
+first_file = true
 file_list.each do |file_hash|
   file_obj = r.TFile.new file_hash[:name], 'READ'
   # get the electron yield, for normalization
   ele_yield = file_obj.Get('dihadronCntDist').GetEntries
   # draw each plot
+  first_plot = true
   plot_list.each do |plot_hash|
     plot_hash[:canv].cd
     plot = file_obj.Get plot_hash[:name]
@@ -72,12 +84,19 @@ file_list.each do |file_hash|
     plot.SetName "#{plot.GetName}_#{file_hash[:id]}"
     plot.SetMarkerStyle file_hash[:style]
     plot.SetMarkerColor file_hash[:color]
-    plot.Draw first ? 'p' : 'p same'
+    plot.SetMarkerSize 1.5
+    plot.GetXaxis.SetRangeUser 0.25, 1.0
+    plot.Draw first_file ? 'p hist' : 'p hist same'
+    legend.AddEntry plot, file_hash[:title], 'p' if first_plot
+    first_plot = false
   end
-  first = false
+  first_file = false
 end
 
 # output plots
 plot_list.each do |plot_hash|
   plot_hash[:canv].SaveAs "ssscomp_#{plot_hash[:name]}.png"
 end
+legend_canv = r.TCanvas.new 'legend', 'legend', 600, 600
+legend.Draw
+legend_canv.SaveAs 'legend.png'
