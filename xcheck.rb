045670ccbbdaf788fbcadedac611c6cbc165cf90
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-09-12 11:31:25 -0400

    feat: another cross check

--- xcheck.rb
+++ xcheck.rb
@@ -16,25 +16,25 @@ sym_arg = ARGV[0]
 
 # open trees
 trees = {
-  :harut   => r.TTree.new,
-  :timothy => r.TTree.new,
-  :chris   => r.TFile.new('outroot.test.inbending/sidisdvcs_016333.hipo.root').Get('tree'),
+  # :harut   => r.TTree.new,
+  :timothy => r.TFile.new('timothy_dihadron_theta_calculation.root').Get('PhysicsEvents'),
+  :chris   => r.TFile.new('outroot.test.inbending/sidisdvcs_016771.hipo.root').Get('tree'),
 }
-[:harut, :timothy].each do |human|
-  trees[human].ReadFile("theta_xcheck.dat")
-end
+# [:harut, :timothy].each do |human|
+#   trees[human].ReadFile("theta_xcheck.dat")
+# end
 
 # cuts
 cuts_options = {
   :symmetric => {
     :harut   => 'TMath::Abs(pipp-pimp) < 1',
-    :timothy => 'TMath::Abs(pipp-pimp) < 1',
-    :chris   => 'evnum < 198878 && TMath::Abs(hadP[0]-hadP[1]) < 1',
+    :timothy => 'TMath::Abs(p1_p-p2_p) < 1',
+    :chris   => 'TMath::Abs(hadP[0]-hadP[1]) < 1 && eleP > 2.64 && Q2 > 1 && W > 2 && y < 0.75',
   },
   :asymmetric => {
     :harut   => 'TMath::Abs(pipp-pimp) > 1',
-    :timothy => 'TMath::Abs(pipp-pimp) > 1',
-    :chris   => 'evnum < 198878 && TMath::Abs(hadP[0]-hadP[1]) > 1',
+    :timothy => 'TMath::Abs(p1_p-p2_p) > 1',
+    :chris   => 'TMath::Abs(hadP[0]-hadP[1]) > 1 && eleP > 2.64 && Q2 > 1 && W > 2 && y < 0.75',
   },
 }
 cuts = cuts_options[sym_arg.to_sym]
@@ -45,14 +45,16 @@ end
 
 # variables and their ranges
 var_hash = {
-  :theta    => { :harut => 'theta_proton_calc', :timothy => 'theta_tim',   :chris => 'theta',                                   :range => [0.0, Math::PI]   },
-  :Mh       => { :harut => 'pippim_mass',       :timothy => 'pippim_mass', :chris => 'Mh',                                      :range => [0.0, 5.0]        },
-  :eleE     => { :harut => 'elee',              :timothy => 'elee',        :chris => 'eleE',                                    :range => [0.0, 11.0]       },
-  :eleTheta => { :harut => 'lelet',             :timothy => 'lelet',       :chris => '2*TMath::ATan(TMath::Exp(-1*eleEta))',    :range => [0.0, Math::PI/4] },
-  :pipP     => { :harut => 'pipp',              :timothy => 'pipp',        :chris => 'hadP[0]',                                 :range => [0.0, 11.0]       },
-  :pipTheta => { :harut => 'pipt',              :timothy => 'pipt',        :chris => '2*TMath::ATan(TMath::Exp(-1*hadEta[0]))', :range => [0.0, Math::PI/4] },
-  :pimP     => { :harut => 'pimp',              :timothy => 'pimp',        :chris => 'hadP[1]',                                 :range => [0.0, 11.0]       },
-  :pimTheta => { :harut => 'pimt',              :timothy => 'pimt',        :chris => '2*TMath::ATan(TMath::Exp(-1*hadEta[1]))', :range => [0.0, Math::PI/4] },
+  :theta    => { :harut => 'theta_proton_calc', :timothy => 'theta',    :chris => 'theta',                                   :range => [0.0, Math::PI]   },
+  :x        => { :harut => '',                  :timothy => 'x',        :chris => 'x',                                       :range => [0.0, 1.0]        },
+  :Q2       => { :harut => '',                  :timothy => 'Q2',       :chris => 'Q2',                                      :range => [0.0, 10.0]       },
+  :Mh       => { :harut => 'pippim_mass',       :timothy => 'Mh',       :chris => 'Mh',                                      :range => [0.0, 5.0]        },
+  :eleE     => { :harut => 'elee',              :timothy => 'e_p',      :chris => 'eleP',                                    :range => [0.0, 11.0]       },
+  :eleTheta => { :harut => 'lelet',             :timothy => 'e_theta',  :chris => '2*TMath::ATan(TMath::Exp(-1*eleEta))',    :range => [0.0, Math::PI/4] },
+  :pipP     => { :harut => 'pipp',              :timothy => 'p1_p',     :chris => 'hadP[0]',                                 :range => [0.0, 11.0]       },
+  :pipTheta => { :harut => 'pipt',              :timothy => 'p1_theta', :chris => '2*TMath::ATan(TMath::Exp(-1*hadEta[0]))', :range => [0.0, Math::PI/4] },
+  :pimP     => { :harut => 'pimp',              :timothy => 'p2_p',     :chris => 'hadP[1]',                                 :range => [0.0, 11.0]       },
+  :pimTheta => { :harut => 'pimt',              :timothy => 'p2_theta', :chris => '2*TMath::ATan(TMath::Exp(-1*hadEta[1]))', :range => [0.0, Math::PI/4] },
 }
 
 # define histograms
@@ -60,7 +62,7 @@ Nbins = 50
 plot_hash = var_hash.keys.map do |var_key|
   plots = trees.keys.map{ |human| [ human, r.TH1D.new("#{var_key.to_s}__#{human.to_s}", "#{var_key.to_s} (#{sym_arg})", Nbins, *var_hash[var_key][:range]) ] }.to_h
   plots[:chris].SetLineColor r.kRed
-  plots[:harut].SetLineColor r.kGreen+1
+  # plots[:harut].SetLineColor r.kGreen+1
   plots[:timothy].SetLineColor r.kBlue
   [ var_key, plots ]
 end.to_h
