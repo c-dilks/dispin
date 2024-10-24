07a3b9e61b975bd30cd19610ca648712a63e8b83
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-10-24 17:21:13 -0400

    feat: define output tree and branches

--- skimDihadrons.cpp
+++ skimDihadrons.cpp
@@ -6,6 +6,7 @@
 #include <iguana/algorithms/AlgorithmSequence.h>
 
 #include <TFile.h>
+#include <TTree.h>
 
 #include "src/Tools.h"
 #include "src/Constants.h"
@@ -13,30 +14,26 @@
 
 int main(int argc, char** argv) {
 
+  // ==================================================================================
   // arguments
+  // ==================================================================================
+
   TString hipoFileName;
+  TString outputDir;
   TString dataStream = "data";
   TString hipoType   = "skim";
-  if(argc == 1) {
-    std::cerr << "USAGE: " << argv[0] << " [hipoFileName] [dataStream] [hipoType]" << std::endl;
+  if(argc < 3) {
+    std::cerr << "USAGE: " << argv[0] << " [hipoFileName] [outputDir] [dataStream] [hipoType]" << std::endl;
     std::cerr << "  [hipoFileName] the HIPO file to analyze" << std::endl;
+    std::cerr << "  [outputDir]    the output dir, for the output `outroot` file" << std::endl;
     std::cerr << "  [dataStream]   'data', 'mcrec', or 'mcgen'; default = " << dataStream << std::endl;
     std::cerr << "  [hipoType]     'skim' or 'dst'; default = " << hipoType << std::endl;
     return 1;
   }
   hipoFileName          = TString(argv[1]);
-  if(argc>2) dataStream = TString(argv[2]);
-  if(argc>3) hipoType   = TString(argv[3]);
-
-  // OPTIONS ///////////////////////////////////////////////////////////////////////
-  bool const verbose = false;
-  //////////////////////////////////////////////////////////////////////////////////
-
-  // define output ROOT file
-  TString diskimFileName = TString("diskim/") + TString(std::filesystem::path(hipoFileName.Data()).filename()) + TString(".root");
-  std::cout << "INPUT FILE:  " << hipoFileName << std::endl;
-  std::cout << "OUTPUT FILE: " << diskimFileName << std::endl;
-  auto diskimFile = new TFile(diskimFileName, "RECREATE");
+  outputDir             = TString(argv[2]);
+  if(argc>3) dataStream = TString(argv[3]);
+  if(argc>4) hipoType   = TString(argv[4]);
 
   // check `dataStream` argument
   //// first check if there are any 'special' strings in `dataStream`
@@ -67,33 +64,25 @@ int main(int argc, char** argv) {
   if(useMC)
     std::cout << "READING MONTE CARLO FILE" << std::endl;
 
+
+  // ==================================================================================
+  // read input file and set input banks
+  // ==================================================================================
+
   // read the input HIPO file, and decide which banks to read
-  hipo::reader hipoReader(hipoFileName.Data(), {0});
-  enum bankEnum {
-    b_event,
-    b_config,
-    b_particle,
-    b_calorimeter,
-    b_track,
-    b_traj,
-    b_mc_particle,
-    b_mc_lund
+  std::vector<std::string> inputBankNames = {
+    "REC::Event",
+    "RUN::config",
+    "REC::Particle",
+    "REC::Calorimeter",
+    "REC::Track",
+    "REC::Traj"
   };
-  std::map<bankEnum,std::string> bankNamesMap;
-  bankNamesMap.insert({b_event,       "REC::Event"});
-  bankNamesMap.insert({b_config,      "RUN::config"});
-  bankNamesMap.insert({b_particle,    "REC::Particle"});
-  bankNamesMap.insert({b_calorimeter, "REC::Calorimeter"});
-  bankNamesMap.insert({b_track,       "REC::Track"});
-  bankNamesMap.insert({b_traj,        "REC::Traj"});
   if(useMCrec) {
-    bankNamesMap.insert({b_mc_particle, "MC::Particle"});
-    bankNamesMap.insert({b_mc_lund,     "MC::Lund"});
+    inputBankNames.push_back("MC::Particle");
+    inputBankNames.push_back("MC::Lund");
   }
-  std::vector<std::string> bankNamesList;
-  for(auto const [_, name] : bankNamesMap)
-    bankNamesList.push_back(name);
-  hipo::banklist hipoBanks = hipoReader.getBanks(bankNamesList);
+  hipo::banklist hipoBanks = hipoReader.getBanks(inputBankNames);
 
 
   // ==================================================================================
@@ -110,6 +99,168 @@ int main(int argc, char** argv) {
 
   iguanaSeq.Start(hipoBanks);
 
+
+  // ==================================================================================
+  // bank indices
+  // ==================================================================================
+
+  //// input banks
+  auto b_event       = iguana::Algorithm::GetBankIndex(banks, "REC::Event");
+  auto b_config      = iguana::Algorithm::GetBankIndex(banks, "RUN::config");
+  auto b_particle    = iguana::Algorithm::GetBankIndex(banks, "REC::Particle");
+  auto b_calorimeter = iguana::Algorithm::GetBankIndex(banks, "REC::Calorimeter");
+  auto b_track       = iguana::Algorithm::GetBankIndex(banks, "REC::Track");
+  auto b_traj        = iguana::Algorithm::GetBankIndex(banks, "REC::Traj");
+  //// input MC banks
+  auto b_mc_particle = useMCrec ? iguana::Algorithm::GetBankIndex(banks, "MC::Particle") : 0;
+  auto b_mc_lund     = useMCrec ? iguana::Algorithm::GetBankIndex(banks, "MC::Lund") : 0;
+  //// output banks
+  auto b_inclusive_kin     = iguana::Algorithm::GetBankIndex(banks, "physics::InclusiveKinematics");
+  auto b_dihadron_kin      = iguana::Algorithm::GetBankIndex(banks, "physics::DihadronKinematics");
+  auto b_single_hadron_kin = iguana::Algorithm::GetBankIndex(banks, "physics::SingleHadronKinematics");
+
+
+  // ==================================================================================
+  // setup output file
+  // ==================================================================================
+
+  TString outputFileName = outputDir + TString("/") + TString(std::filesystem::path(hipoFileName.Data()).filename()) + TString(".root");
+  std::cout << "INPUT FILE:  " << hipoFileName << std::endl;
+  std::cout << "OUTPUT FILE: " << outputFileName  << std::endl;
+  auto outputFile = new TFile(outputFileName , "RECREATE");
+
+  auto outTree = new TTree("tree", "tree");
+  // - DIS kinematics branches
+  Float_t W{0};     outrootTr->Branch("W",     &W,      "W/F");
+  Float_t Q2{0};    outrootTr->Branch("Q2",    &Q2,     "Q2/F");
+  Float_t Nu{0};    outrootTr->Branch("Nu",    &Nu,     "Nu/F");
+  Float_t x{0};     outrootTr->Branch("x",     &x,      "x/F");
+  Float_t y{0};     outrootTr->Branch("y",     &y,      "y/F");
+  Float_t beamE{0}; outrootTr->Branch("beamE", &BeamEn, "beamE/F");
+  // - electron kinematics branches
+  Float_t eleE{0};             outrootTr->Branch("eleE",       &eleE,       "eleE/F");
+  Float_t eleP{0};             outrootTr->Branch("eleP",       &eleP,       "eleP/F");
+  Float_t elePt{0};            outrootTr->Branch("elePt",      &elePt,      "elePt/F");
+  Float_t eleEta{0};           outrootTr->Branch("eleEta",     &eleEta,     "eleEta/F");
+  Float_t elePhi{0};           outrootTr->Branch("elePhi",     &elePhi,     "elePhi/F");
+  Float_t eleVertex[3]{0,0,0}; outrootTr->Branch("eleVertex",  eleVertex,   "eleVertex[3]/F");
+  Int_t   eleStatus{0};        outrootTr->Branch("eleStatus",  &eleStatus,  "eleStatus/I");
+  Float_t eleChi2pid{0};       outrootTr->Branch("eleChi2pid", &eleChi2pid, "eleChi2pid/F");
+  Bool_t  eleFiduCut{false};   outrootTr->Branch("eleFiduCut", &eleFiduCut, "eleFiduCut/O");
+  Float_t elePCALen{0};        outrootTr->Branch("elePCALen",  &elePCALen,  "elePCALen/F");
+  Float_t eleECINen{0};        outrootTr->Branch("eleECINen",  &eleECINen,  "eleECINen/F");
+  Float_t eleECOUTen{0};       outrootTr->Branch("eleECOUTen", &eleECOUTen, "eleECOUTen/F");
+  Int_t   eleSector{0};        outrootTr->Branch("eleSector",  &eleSector,  "eleSector/I");
+  // - hadron branches
+  Int_t   pairType{0};                      outrootTr->Branch("pairType",   &pairType,  "pairType/I");
+  Int_t   hadRow[2]{0,0};                   outrootTr->Branch("hadRow",     hadRow,     "hadRow[2]/I");
+  Int_t   hadIdx[2]{0,0};                   outrootTr->Branch("hadIdx",     hadIdx,     "hadIdx[2]/I");
+  Float_t hadE[2]{0,0};                     outrootTr->Branch("hadE",       hadE,       "hadE[2]/F");
+  Float_t hadP[2]{0,0};                     outrootTr->Branch("hadP",       hadP,       "hadP[2]/F");
+  Float_t hadPt[2]{0,0};                    outrootTr->Branch("hadPt",      hadPt,      "hadPt[2]/F");
+  Float_t hadEta[2]{0,0};                   outrootTr->Branch("hadEta",     hadEta,     "hadEta[2]/F");
+  Float_t hadPhi[2]{0,0};                   outrootTr->Branch("hadPhi",     hadPhi,     "hadPhi[2]/F");
+  Float_t hadXF[2]{0,0};                    outrootTr->Branch("hadXF",      hadXF,      "hadXF[2]/F");
+  Float_t hadVertex[2][3]{{0,0,0},{0,0,0}}; outrootTr->Branch("hadVertex",  hadVertex,  "hadVertex[2][3]/F");
+  Int_t   hadStatus[2]{0,0};                outrootTr->Branch("hadStatus",  hadStatus,  "hadStatus[2]/I");
+  Float_t hadBeta[2]{0,0};                  outrootTr->Branch("hadBeta",    hadBeta,    "hadBeta[2]/F");
+  Float_t hadChi2pid[2]{0,0};               outrootTr->Branch("hadChi2pid", hadChi2pid, "hadChi2pid[2]/F");
+  Bool_t  hadFiduCut[2]{false,false};       outrootTr->Branch("hadFiduCut", hadFiduCut, "hadFiduCut[2]/O");
+  // - dihadron branches
+  Float_t Mh{0};      outrootTr->Branch("Mh",      &Mh,      "Mh/F");
+  Float_t Mmiss{0};   outrootTr->Branch("Mmiss",   &Mmiss,   "Mmiss/F");
+  Float_t Z[2]{0,0};  outrootTr->Branch("Z",       Z,        "Z[2]/F");
+  Float_t Zpair{0};   outrootTr->Branch("Zpair",   &Zpair,   "Zpair/F");
+  Float_t xF{0};      outrootTr->Branch("xF",      &xF,      "xF/F");
+  Float_t alpha{0};   outrootTr->Branch("alpha",   &alpha,   "alpha/F");
+  Float_t theta{0};   outrootTr->Branch("theta",   &theta,   "theta/F");
+  Float_t thetaLI{0}; outrootTr->Branch("thetaLI", &thetaLI, "thetaLI/F");
+  Float_t zeta{0};    outrootTr->Branch("zeta",    &zeta,    "zeta/F");
+  Float_t Ph{0};      outrootTr->Branch("Ph",      &Ph,      "Ph/F");
+  Float_t PhPerp{0};  outrootTr->Branch("PhPerp",  &PhPerp,  "PhPerp/F");
+  Float_t PhEta{0};   outrootTr->Branch("PhEta",   &PhEta,   "PhEta/F");
+  Float_t PhPhi{0};   outrootTr->Branch("PhPhi",   &PhPhi,   "PhPhi/F");
+  Float_t R{0};       outrootTr->Branch("R",       &R,       "R/F");
+  Float_t RPerp{0};   outrootTr->Branch("RPerp",   &RPerp,   "RPerp/F");
+  Float_t RT{0};      outrootTr->Branch("RT",      &RT,      "RT/F");
+  Float_t PhiH{0};    outrootTr->Branch("PhiH",    &PhiH,    "PhiH/F");
+  // -- phiR angles
+  Float_t PhiRq{0};   outrootTr->Branch("PhiRq",   &PhiRq,   "PhiRq/F");
+  Float_t PhiRp{0};   outrootTr->Branch("PhiRp",   &PhiRp,   "PhiRp/F");
+  Float_t PhiRp_r{0}; outrootTr->Branch("PhiRp_r", &PhiRp_r, "PhiRp_r/F");
+  Float_t PhiRp_g{0}; outrootTr->Branch("PhiRp_g", &PhiRp_g, "PhiRp_g/F");
+  // -- SDME kinematics
+  Float_t sdmePhiU{0}; outrootTr->Branch("sdmePhiU", &sdmePhiU, "sdmePhiU/F");
+  Float_t sdmePhiL{0}; outrootTr->Branch("sdmePhiL", &sdmePhiL, "sdmePhiL/F");
+  // - event-level branches
+  Int_t runnum{0};   outrootTr->Branch("runnum",   &runnum,   "runnum/I");
+  Int_t evnum{0};    outrootTr->Branch("evnum",    &evnum,    "evnum/I");
+  Int_t helicity{0}; outrootTr->Branch("helicity", &helicity, "helicity/I");
+  // - MC branches
+  if(useMCrec) {
+    // - generated DIS kinematics branches
+    Float_t gen_W{0};  outrootTr->Branch("gen_W",  &gen_W,  "gen_W/F");
+    Float_t gen_Q2{0}; outrootTr->Branch("gen_Q2", &gen_Q2, "gen_Q2/F");
+    Float_t gen_Nu{0}; outrootTr->Branch("gen_Nu", &gen_Nu, "gen_Nu/F");
+    Float_t gen_x{0};  outrootTr->Branch("gen_x",  &gen_x,  "gen_x/F");
+    Float_t gen_y{0};  outrootTr->Branch("gen_y",  &gen_y,  "gen_y/F");
+    // - generated electron kinematics branches
+    Float_t gen_eleE{0};             outrootTr->Branch("gen_eleE",      &gen_eleE,     "gen_eleE/F");
+    Float_t gen_eleP{0};             outrootTr->Branch("gen_eleP",      &gen_eleP,     "gen_eleP/F");
+    Float_t gen_elePt{0};            outrootTr->Branch("gen_elePt",     &gen_elePt,    "gen_elePt/F");
+    Float_t gen_eleEta{0};           outrootTr->Branch("gen_eleEta",    &gen_eleEta,   "gen_eleEta/F");
+    Float_t gen_elePhi{0};           outrootTr->Branch("gen_elePhi",    &gen_elePhi,   "gen_elePhi/F");
+    Float_t gen_eleVertex[3]{0,0,0}; outrootTr->Branch("gen_eleVertex", gen_eleVertex, "gen_eleVertex[3]/F");
+    // - generated hadron branches
+    Int_t   gen_pairType{0};                      outrootTr->Branch("gen_pairType",  &gen_pairType, "gen_pairType/I");
+    Int_t   gen_hadRow[2]{0,0};                   outrootTr->Branch("gen_hadRow",    gen_hadRow,    "gen_hadRow[2]/I");
+    Int_t   gen_hadIdx[2]{0,0};                   outrootTr->Branch("gen_hadIdx",    gen_hadIdx,    "gen_hadIdx[2]/I");
+    Float_t gen_hadE[2]{0,0};                     outrootTr->Branch("gen_hadE",      gen_hadE,      "gen_hadE[2]/F");
+    Float_t gen_hadP[2]{0,0};                     outrootTr->Branch("gen_hadP",      gen_hadP,      "gen_hadP[2]/F");
+    Float_t gen_hadPt[2]{0,0};                    outrootTr->Branch("gen_hadPt",     gen_hadPt,     "gen_hadPt[2]/F");
+    Float_t gen_hadEta[2]{0,0};                   outrootTr->Branch("gen_hadEta",    gen_hadEta,    "gen_hadEta[2]/F");
+    Float_t gen_hadPhi[2]{0,0};                   outrootTr->Branch("gen_hadPhi",    gen_hadPhi,    "gen_hadPhi[2]/F");
+    Float_t gen_hadXF[2]{0,0};                    outrootTr->Branch("gen_hadXF",     gen_hadXF,     "gen_hadXF[2]/F");
+    Float_t gen_hadVertex[2][3]{{0,0,0},{0,0,0}}; outrootTr->Branch("gen_hadVertex", gen_hadVertex, "gen_hadVertex[2][3]/F");
+    // - generated dihadron branches
+    Float_t gen_Mh{0};      outrootTr->Branch("gen_Mh",      &gen_Mh,      "gen_Mh/F");
+    Float_t gen_Mmiss{0};   outrootTr->Branch("gen_Mmiss",   &gen_Mmiss,   "gen_Mmiss/F");
+    Float_t gen_Z[2]{0,0};  outrootTr->Branch("gen_Z",       gen_Z,        "gen_Z[2]/F");
+    Float_t gen_Zpair{0};   outrootTr->Branch("gen_Zpair",   &gen_Zpair,   "gen_Zpair/F");
+    Float_t gen_xF{0};      outrootTr->Branch("gen_xF",      &gen_xF,      "gen_xF/F");
+    Float_t gen_alpha{0};   outrootTr->Branch("gen_alpha",   &gen_alpha,   "gen_alpha/F");
+    Float_t gen_theta{0};   outrootTr->Branch("gen_theta",   &gen_theta,   "gen_theta/F");
+    Float_t gen_zeta{0};    outrootTr->Branch("gen_zeta",    &gen_zeta,    "gen_zeta/F");
+    Float_t gen_Ph{0};      outrootTr->Branch("gen_Ph",      &gen_Ph,      "gen_Ph/F");
+    Float_t gen_PhPerp{0};  outrootTr->Branch("gen_PhPerp",  &gen_PhPerp,  "gen_PhPerp/F");
+    Float_t gen_PhEta{0};   outrootTr->Branch("gen_PhEta",   &gen_PhEta,   "gen_PhEta/F");
+    Float_t gen_PhPhi{0};   outrootTr->Branch("gen_PhPhi",   &gen_PhPhi,   "gen_PhPhi/F");
+    Float_t gen_R{0};       outrootTr->Branch("gen_R",       &gen_R,       "gen_R/F");
+    Float_t gen_RPerp{0};   outrootTr->Branch("gen_RPerp",   &gen_RPerp,   "gen_RPerp/F");
+    Float_t gen_RT{0};      outrootTr->Branch("gen_RT",      &gen_RT,      "gen_RT/F");
+    Float_t gen_PhiH{0};    outrootTr->Branch("gen_PhiH",    &gen_PhiH,    "gen_PhiH/F");
+    Float_t gen_PhiRq{0};   outrootTr->Branch("gen_PhiRq",   &gen_PhiRq,   "gen_PhiRq/F");
+    Float_t gen_PhiRp{0};   outrootTr->Branch("gen_PhiRp",   &gen_PhiRp,   "gen_PhiRp/F");
+    Float_t gen_PhiRp_r{0}; outrootTr->Branch("gen_PhiRp_r", &gen_PhiRp_r, "gen_PhiRp_r/F");
+    Float_t gen_PhiRp_g{0}; outrootTr->Branch("gen_PhiRp_g", &gen_PhiRp_g, "gen_PhiRp_g/F");
+    // - match quality
+    Bool_t  gen_eleIsMatch{false};          outrootTr->Branch("gen_eleIsMatch",   &gen_eleIsMatch,   "gen_eleIsMatch/O");
+    Bool_t  gen_hadIsMatch[2]{false,false}; outrootTr->Branch("gen_hadIsMatch",   gen_hadIsMatch,    "gen_hadIsMatch[2]/O");
+    Float_t gen_eleMatchDist{0};            outrootTr->Branch("gen_eleMatchDist", &gen_eleMatchDist, "gen_eleMatchDist/F");
+    Float_t gen_hadMatchDist[2]{0,0};       outrootTr->Branch("gen_hadMatchDist", gen_hadMatchDist,  "gen_hadMatchDist[2]/F");
+  }
+  // - parent info
+  if(useMCrec || useMCgen || useStringSpinner) {
+    Int_t gen_hadParentIdx[2]{0,0}; outrootTr->Branch("gen_hadParentIdx", gen_hadParentIdx, "gen_hadParentIdx[2]/I");
+    Int_t gen_hadParentPid[2]{0,0}; outrootTr->Branch("gen_hadParentPid", gen_hadParentPid, "gen_hadParentPid[2]/I");
+  }
+  // - StringSpinner branches
+  if(useStringSpinner) {
+    Float_t SS_Q2{0}; outrootTr->Branch("SS_Q2", &SS_Q2, "SS_Q2/F");
+    Float_t SS_W{0};  outrootTr->Branch("SS_W",  &SS_W,  "SS_W/F");
+    Float_t SS_x{0};  outrootTr->Branch("SS_x",  &SS_x,  "SS_x/F");
+    Float_t SS_y{0};  outrootTr->Branch("SS_y",  &SS_y,  "SS_y/F");
+  }
 
 
   // ==================================================================================
