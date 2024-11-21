e61e11bdea7ddb4bd839c137662c0b11e48c3f72
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-11-21 17:45:48 -0500

    feat: fit the kT distribution for sigma

--- drawkt.C
+++ drawkt.C
@@ -2,5 +2,8 @@ void drawkt(TString fn="diskim.sss/test.root") { // draw the kT distribution fro
   auto* f = new TFile(fn,"READ");
   auto* t = (TTree*) f->Get("ditr");
   new TCanvas();
-  t->Draw("SS_kT","SS_kT>-5000");
+  auto m = TMath::Power(t->GetMaximum("SS_kT"), 2);
+  auto d = new TH1D("kT", "kT", 100, 0, m);
+  t->Project("kT", "TMath::Power(SS_kT,2)", "SS_kT>-5000");
+  d->Fit("gaus", "", "", 0, m);
 }
